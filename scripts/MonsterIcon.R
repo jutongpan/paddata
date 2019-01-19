b1989ff163cc9da0ff620c788d3c7eaad1571268
Jutong Pan <panjutong@gmail.com>
2019-01-19 16:49:02 -0500

    Update MonsterIcon.R to fit new directory structure

--- scripts/MonsterIcon.R
+++ scripts/MonsterIcon.R
@@ -3,19 +3,20 @@ library(data.table)
 library(DBI)
 
 if (Sys.info()[["nodename"]] == "JUTONG-X1C") {
-  setwd("C:/Users/Jutong/Documents/padmonster")
+  setwd("C:/Users/Jutong/Documents/paddata")
+  path_MonsterIcon <- "C:/Users/Jutong/Documents/padmonster/img/MonsterIcon/"
 } else {
-  setwd("//Users/yawenliang/Documents/PadMonster")
+  setwd("//Users/yawenliang/Documents/paddata")
+  path_MonsterIcon <- "//Users/yawenliang/Documents/padmonster/img/MonsterIcon/"
 }
 
-conn <- dbConnect(drv = RSQLite::SQLite(), "db/padmonster.sqlite3")
-Monster.db <- dbReadTable(conn, "Monster")
-Monster.dt <- data.table(Monster.db)
-MonsterIcon.dt <- Monster.dt[ , c("MonsterId", "MonsterIconPathDownload")]
+conn <- dbConnect(drv = RSQLite::SQLite(), "padmonster.sqlite3")
+Monster.dt <- setDT(dbReadTable(conn, "Monster"))
+MonsterIcon.dt <- Monster.dt[ , c("MonsterId", "MonsterIconDownload")]
 for (i in 1:nrow(MonsterIcon.dt)){
-  if (file.exists(paste0("app/img/MonsterIcon/", MonsterIcon.dt[i, MonsterId], ".png")) == FALSE) {
-      download.file(MonsterIcon.dt$MonsterIconPathDownload[i],
-                    paste0("app/img/MonsterIcon/", MonsterIcon.dt[i, MonsterId], ".png"),
+  if (file.exists(paste0(path_MonsterIcon, MonsterIcon.dt[i, MonsterId], ".png")) == FALSE) {
+      download.file(MonsterIcon.dt$MonsterIconDownload[i],
+                    paste0(path_MonsterIcon, MonsterIcon.dt[i, MonsterId], ".png"),
                     mode = "wb")
   }
 }
