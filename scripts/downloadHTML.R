354a8445c8ec0391f29edbf22e308f37ca4bb32f
Jutong Pan <panjutong@gmail.com>
2019-01-21 22:19:15 -0500

    Add downloading page of Active Skills by type

--- scripts/downloadHTML.R
+++ scripts/downloadHTML.R
@@ -11,3 +11,7 @@ for (id in 1:5074) {
   if (id %% 100 == 0) Sys.sleep(30)
   if (id %% 10 == 0) Sys.sleep(3)
 }
+
+## Download page of active skills by type
+path <- "http://pad.skyozora.com/skill/%E4%B8%BB%E5%8B%95%E6%8A%80%E8%83%BD%E4%B8%80%E8%A6%BD/"
+download.file(path, "raw/ActiveSkillType.html")
