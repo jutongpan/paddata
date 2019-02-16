994645f9abd4f04177cf8783037f40d560f87376
Jutong Pan <panjutong@gmail.com>
2019-02-16 16:22:50 -0500

    Update AwokenSkill table with new name for L heart. Update AwokenSkill.R. Update htmls for monsters with L heart awoken skills.

--- scripts/AwokenSkill.R
+++ scripts/AwokenSkill.R
@@ -4,7 +4,11 @@ library(rvest)
 library(DBI)
 library(RSQLite)
 
-setwd("//Users/yawenliang/Documents/PadMonster")
+if (Sys.info()[["nodename"]] == "JUTONG-X1C") {
+  setwd("C:/Users/Jutong/Documents/paddata")
+} else {
+  setwd("//Users/yawenliang/Documents/paddata")
+}
 
 ## 1. Scrape awoken skill data
 
@@ -21,28 +25,15 @@ AwokenSkillIconDownload.dt <- data.table(AwokenSkillIconDownload)
 AwokenSkill.dt <- cbind(AwokenSkillName.dt, AwokenSkillIconDownload.dt)
 AwokenSkill.dt <- AwokenSkill.dt[-1,]
 AwokenSkill.dt[ , AwokenSkillId := 1:nrow(AwokenSkill.dt)]
-#AwokenSkill.dt[ ,AwokenSkillIconPath := paste0("img/AwokenSkill/", AwokenSkillId,".png")]
 
-
-for(i in 1:length(AwokenSkill.dt$AwokenSkillIconDownload)){
-download.file(paste0(AwokenSkill.dt$AwokenSkillIconDownload[i]), 
-              paste0("app/img/AwokenSkill/", AwokenSkill.dt[i,AwokenSkillId], ".png"))
-}
-
+# for(i in 1:length(AwokenSkill.dt$AwokenSkillIconDownload)){
+# download.file(paste0(AwokenSkill.dt$AwokenSkillIconDownload[i]),
+#               paste0("app/img/AwokenSkill/", AwokenSkill.dt[i,AwokenSkillId], ".png"))
+# }
 
 
 ## 2. Write the data into database
-conn <- dbConnect(drv = RSQLite::SQLite(), "db/padmonster.sqlite3")
-
-# a <- dbListTables(conn)
-# for (i in 1:length(a)) {
-#   assign(a[i], value = dbReadTable(conn, a[i]))
-# }
-# AwokenSkill <- dbReadTable(conn, "AwokenSkill")
-
-AwokenSkill.dt[, AwokenSkillDescription := NA]
-AwokenSkill.dt[, Id:= 1:nrow(AwokenSkill.dt) ]
-setcolorder(AwokenSkill.dt, c("Id", "AwokenSkillId"))
-
-dbWriteTable(conn, "AwokenSkill", AwokenSkill.dt, overwrite = TRUE)
-dbDisconnect(conn) 
+conn <- dbConnect(drv = RSQLite::SQLite(), "padmonster.sqlite3")
+dbExecute(conn, "DELETE FROM AwokenSkill")
+dbWriteTable(conn, "AwokenSkill", AwokenSkill.dt, append = TRUE)
+dbDisconnect(conn)
