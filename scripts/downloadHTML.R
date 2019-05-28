718def7355ef0a41a83df61841b769437043ee3b
Jutong Pan <panjutong@gmail.com>
2019-05-28 02:21:11 +0000

    Write a function to extract monster IDs mentioned in news.

--- scripts/downloadHTML.R
+++ scripts/downloadHTML.R
@@ -1,6 +1,25 @@
+library(data.table)
+library(rvest)
+library(stringr)
+
 setwd("C:/Users/Jutong/Documents/paddata")
 
-id.vt <- c(5129:5134, 5154)
+extractMonIdsToScrape <- function(link) {
+  webpage <- read_html(link)
+  hrefs <- html_nodes(webpage, "a") %>% html_attr("href")
+  hrefs_pets <- str_match(string = hrefs, pattern = "^pets/([0-9]{3,4})$")[,2]
+  vt_id <- as.integer(hrefs_pets[!is.na(hrefs_pets)])
+  vt_id
+}
+
+vt_link <- c(
+  "http://pad.skyozora.com/",
+  "http://pad.skyozora.com/news/%EF%BC%8805/26%E5%AF%A6%E8%A3%9D%EF%BC%89%E9%83%A8%E4%BB%BD%E5%AF%B5%E7%89%A9PowerUp%EF%BC%81",
+  "http://pad.skyozora.com/news/%EF%BC%8805/26%E5%AF%A6%E8%A3%9D%EF%BC%89%E9%83%A8%E4%BB%BD%E5%AF%B5%E7%89%A9%E8%BF%BD%E5%8A%A0%E9%80%B2%E5%8C%96%E5%BD%A2%E6%85%8B%EF%BC%81"
+)
+
+id.vt <- unlist(sapply(vt_link, extractMonIdsToScrape), use.names = F)
+
 index <- 1
 while (index <= length(id.vt)) {
   tryCatch(
