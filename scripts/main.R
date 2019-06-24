9a65c45442ff7de5967d60b36d79e1774b2b75d5
Jutong Pan <panjutong@gmail.com>
2019-06-24 13:39:56 -0400

    Added main.R for sourcing all scripts in one run.

---
+++ scripts/main.R
@@ -0,0 +1,12 @@
+if (Sys.info()[["nodename"]] == "JUTONG-X1C") {
+  setwd("C:/Users/Jutong/Documents/paddata")
+} else if (Sys.info()[["nodename"]] == "MU-JPAN") {
+  setwd("C:/Users/JPan/Documents/repo/paddata")
+} else if (Sys.info()[["nodename"]] == "jpan-personal") {
+  setwd("/home/jpan/paddata")
+}
+
+source(file = "scripts/downloadHTML.R", local = T)
+source(file = "scripts/scraper.R", local = T)
+source(file = "scripts/MonsterIcon.R", local = T)
+source(file = "scripts/ActiveSkillType.R", local = T)
