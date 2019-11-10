780d64023c55d1e6701c6d2e3663bd4090f3de98
Jutong Pan <panjutong@gmail.com>
2019-11-10 21:18:56 +0000

    Update db for Long Yue Shi.

--- scripts/downloadHTML.R
+++ scripts/downloadHTML.R
@@ -22,7 +22,7 @@ extractMonIdsToScrape <- function(link) {
 
 vt_link <- c(
   "http://pad.skyozora.com/"#,
-  # "https://pad.skyozora.com/news/（10/23實裝）部份寵物追加進化形態＆能力調整！"
+  # "https://pad.skyozora.com/news/（11/08實裝）部份寵物追加進化形態＆能力調整！"
 )
 
 id.vt <- unique(unlist(sapply(vt_link, extractMonIdsToScrape), use.names = F))
